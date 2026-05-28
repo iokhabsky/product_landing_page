7e141a7845ceaea4c3b97feb65f0b7168320d911
ivan <iokhabsky@gmail.com>
2026-05-28 15:48:13 +0300

    animation and hover fixes

--- js/common.js
+++ js/common.js
@@ -1,8 +1,55 @@
 window.onload = () => {
-    console.log('loaded');
+    const linePath = document.querySelector('.road_progress');
+    const lineLength = linePath.getTotalLength();
+    const allSteps = document.querySelectorAll(".s-process-steps .step");
+
+    linePath.style.strokeDasharray = lineLength;
+    linePath.style.strokeDashoffset = lineLength;
+
+    linePath.style.opacity = 1;
+
+    function animateLine() {
+        let currStep = 0;
+        setTimeout(() => {
+            linePath.style.strokeDashoffset = lineLength * 0.66;
+            allSteps[currStep].classList.add("active");
+            currStep++;
+        }, 0);
+
+        setTimeout(() => {
+            linePath.style.strokeDashoffset = lineLength * 0.33;
+            allSteps[currStep].classList.add("active");
+            currStep++;
+        }, 1200);
+
+        setTimeout(() => {
+            linePath.style.strokeDashoffset = 0;
+            allSteps[currStep].classList.add("active");
+            currStep++;
+        }, 2400);
+    }
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    animateLine();
+                    observer.unobserve(entry.target);
+                }
+            });
+        },
+        {
+            threshold: 0.55,
+        }
+    );
+
+    observer.observe(document.querySelector(".s-process"));
+
     setUI();
 
     function setUI() {
+
+        // Slider code
         const swiper = new Swiper('.swiper', {
             direction: 'horizontal',
             loop: true,
@@ -10,6 +57,8 @@ window.onload = () => {
                 nextEl: '.custom-next',
                 prevEl: '.custom-prev',
             },
-        });
+        }); //
+
+
     }
 }
